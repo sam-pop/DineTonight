133a829a88702d094a11a203a9e81f4d60cdd12e
Samuel Poplovitch <poplovitch.samuel@gmail.com>
2018-10-22 10:58:54 -0400

    navbar added

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+
+class Navbar extends Component {
+  render() {
+    return (
+      <div>
+        <nav className="navbar">
+          <div className="navbar-brand">
+            <a className="navbar-item  navbar-start">logo-will-come-here</a>
+            <a className="navbar-burger" data-target="navMenu">
+              <span />
+              <span />
+              <span />
+            </a>
+          </div>
+
+          <div className="navbar-menu" id="navMenu">
+            <a className="navbar-item">About</a>
+            <a className="navbar-item">Sign-in</a>
+            <a className="navbar-item">Contact</a>
+            <a className="navbar-item navbar-end">
+              <i className="fab fa-github" style={{ "font-size": "2rem" }} />
+            </a>
+          </div>
+        </nav>
+      </div>
+    );
+  }
+}
+
+export default Navbar;
