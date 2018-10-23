77ef5314b5db079a4e0a3545c6a4c6f9f337ec2a
Samuel Poplovitch <poplovitch.samuel@gmail.com>
2018-10-23 14:25:56 -0400

    refactoring

--- client/src/App.js
+++ client/src/App.js
@@ -24,11 +24,11 @@ class App extends Component {
         <h1>Looking for a place to eat?</h1>
         <h2>Don't "roll the dice" on a restuarant... let us do it for you!</h2>
         <section className="container" style={styles.container}>
-          <div className="has-text-left">
+          <div className="has-text-left"><br/>
             We can show you a <u>top-rating</u> restuarnt near your current
-            location with a simple click of a button.
+            location with a simple click of a button!
           </div>{" "}
-          <div className="has-text-right">text</div>
+          <div className="has-text-right"><br/>So why won't you let us make the tough decisions, leaving you with more time to relax.</div>
         </section>
         <div className="has-text-centered">
           {this.state.firstRun ? <DiceButton default="true" /> : <DiceButton />}
