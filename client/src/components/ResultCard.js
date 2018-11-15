88c5285edc7163f43a73c500acae5f5b9f4bb917
Sam Pop <poplovitch.samuel@gmail.com>
2018-11-15 11:44:34 -0500

    component refactoring

--- client/src/components/ResultCard.js
+++ client/src/components/ResultCard.js
@@ -12,31 +12,25 @@ class ResultCard extends React.Component {
     this.setState({ currentResult: nextProps.currentResult });
   }
 
-  renderDollarSigns = () => {
+  renderDollarSigns = num => {
     let res = "";
-    for (let i = 0; i < this.props.currentResult.restaurant.price_range; i++) {
+    for (let i = 0; i < num; i++) {
       res += "$";
     }
     return res;
   };
 
   render() {
-    const rName = this.props.currentResult.restaurant.name;
-    const rCuisines = this.props.currentResult.restaurant.cuisines;
-    const rFeaturedImage = this.props.currentResult.restaurant.featured_image;
-    const rAddress = this.props.currentResult.restaurant.location.address;
-    const rAddressLink = "https://maps.google.com/?q=" + rAddress;
-    const rMenuUrl = this.props.currentResult.restaurant.menu_url;
-    const rUserRating = this.props.currentResult.restaurant.user_rating
-      .aggregate_rating;
-    const rNumVotes = this.props.currentResult.restaurant.user_rating.votes;
-    const rPriceRange = this.props.currentResult.restaurant.price_range;
+    const thisResult = this.props.currentResult.restaurant;
+    const gmAddressLink =
+      "https://maps.google.com/?q=" + thisResult.location.address;
 
     return (
       <div
         className="card"
         style={{
           width: "70%",
+          maxHeight: "50vh",
           margin: "0 auto",
           marginTop: "7%",
           textAlign: "center"
@@ -44,21 +38,23 @@ class ResultCard extends React.Component {
       >
         <div className="card-content">
           <p className="title">
-            <u>{rName}</u>
+            <u>{thisResult.name}</u>
             <span style={{ fontSize: "0.6em" }}>
               &nbsp;&nbsp;
-              <i>{rUserRating}</i> / 5
+              <i>{thisResult.user_rating.aggregate_rating}</i> / 5
               {/* <span class="icon">
                 <i class="fas fa-star" />
               </span> */}
               &nbsp;
-              <span style={{ fontSize: "0.7em" }}>({rNumVotes} votes)</span>
+              <span style={{ fontSize: "0.7em" }}>
+                ({thisResult.user_rating.votes} votes)
+              </span>
             </span>
             <br />
             <span style={{ fontSize: "0.6em" }}>
-              {this.renderDollarSigns()}
+              {this.renderDollarSigns(thisResult.price_range)}
               &nbsp;-&nbsp;
-              {rCuisines}
+              {thisResult.cuisines}
             </span>
             <br />
           </p>
@@ -66,11 +62,16 @@ class ResultCard extends React.Component {
             className="subtitle"
             style={{ textAlign: "center", margin: "0 auto" }}
           >
-            <img src={rFeaturedImage} width="90%" />
+            <div
+              className="img-container"
+              style={{ margin: "0 auto", width: "50%", marginBottom: "10%" }}
+            >
+              <img src={thisResult.featured_image} />
+            </div>
             <br />
             <br />
-            <a href={rAddressLink} target="_blank">
-              {rAddress}
+            <a href={gmAddressLink} target="_blank">
+              {thisResult.location.address}
             </a>
             <br />
             <span style={{ fontSize: "0.6em" }}>
@@ -84,7 +85,7 @@ class ResultCard extends React.Component {
         <footer className="card-footer">
           <p className="card-footer-item">
             <span>
-              <a href={rMenuUrl} target="_blank">
+              <a href={thisResult.menu_url} target="_blank">
                 View Menu
               </a>
             </span>
