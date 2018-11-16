35c2acff8c2e1e28475e12b14f176f9c53016d42
Sam Pop <poplovitch.samuel@gmail.com>
2018-11-16 16:35:55 -0500

    extracting zomato object data

--- routes/api/restaurants.js
+++ routes/api/restaurants.js
@@ -6,7 +6,7 @@ const request = require("request-promise-native");
 router.route("/").post((req, res) => {
   let zomatoBODY;
   let yelpBODY;
-
+  let ready = false;
   // Current location is passed through the req
   if (req.body) {
     if (req.body.lat && req.body.lon) {
@@ -39,10 +39,11 @@ router.route("/").post((req, res) => {
                 rating: Obj.restaurant.user_rating.aggregate_rating,
                 votes: Obj.restaurant.user_rating.votes,
                 menu: Obj.restaurant.menu_url,
-                image: Obj.restaurant.featured_image
+                image: Obj.restaurant.featured_image,
+                phone: Obj.restaurant.phone_numbers
               };
             });
-            console.log(JSON.stringify(zomatoBODY, null, 4));
+            // console.log(JSON.stringify(apiRes, null, 4));
             return true;
           })
           .catch(err => console.log(err));
@@ -56,14 +57,31 @@ router.route("/").post((req, res) => {
           json: true
         })
           .then(yelpRes => {
-            yelpBODY = yelpRes;
+            //Extracting relevant properties from the API response
+            yelpBODY = yelpRes.businesses.map(Obj => {
+              let thisCuisines = Obj.categories.map(Cat => {
+                return Cat.title;
+              });
+              return {
+                name: Obj.name,
+                cuisines: thisCuisines.toString(),
+                address: Obj.location.display_address[0],
+                price_range: Obj.price,
+                rating: Obj.rating,
+                votes: Obj.review_count,
+                link: Obj.url,
+                image: Obj.image_url,
+                phone: Obj.display_phone
+              };
+            });
+            // console.log(JSON.stringify(yelpBODY, null, 4));
             return true;
           })
           .catch(err => console.log(err));
         //After both requests are done
         if (zomato && yelp) {
           console.log("we got both working!");
-          res.json(zomatoBODY);
+          // res.json(zomatoBODY);
         }
       }
       getRemoteData();
