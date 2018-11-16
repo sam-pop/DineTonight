4d2099423cfb8e00daf19ee94e5e431065949b24
Sam Pop <poplovitch.samuel@gmail.com>
2018-11-16 11:00:49 -0500

    added Yelp backend integration and authentication

--- routes/api/restaurants.js
+++ routes/api/restaurants.js
@@ -4,13 +4,19 @@ const request = require("request");
 
 // Matches with "/api/restaurants"
 router.route("/").post((req, res) => {
+  let zomatoBODY;
+  let yelpBODY;
+
   // Current location is passed through the req
   if (req.body) {
     if (req.body.lat && req.body.lon) {
+      //Coordinates
       let lat = parseFloat(req.body.lat);
       let lon = parseFloat(req.body.lon);
-      //API call to Zomato with the current coordinates
-      const zomatoURL = `https://developers.zomato.com/api/v2.1/geocode?lat=${lat}&lon=${lon}&start=0&count=20`;
+      // Construct API urls
+      const zomatoURL = `https://developers.zomato.com/api/v2.1/geocode?lat=${lat}&lon=${lon}`;
+      const yelpURL = ` https://api.yelp.com/v3/businesses/search?term=restaurants&latitude=${lat}&longitude=${lon}&open_now=true&radius=1000`;
+      //API call to Zomato
       request(
         {
           headers: {
@@ -19,7 +25,22 @@ router.route("/").post((req, res) => {
           uri: zomatoURL
         },
         (error, response, body) => {
-          return res.json(JSON.parse(body));
+          zomatoBODY = JSON.parse(body);
+          // return res.json(JSON.parse(body));
+        }
+      );
+      //API call to Yelp
+      request(
+        {
+          headers: {
+            Authorization: `Bearer ${process.env.YELP_API}`
+          },
+          uri: yelpURL
+        },
+        (error, response, body) => {
+          yelpBODY = JSON.parse(body);
+          console.log(yelpBODY);
+          // return res.json(JSON.parse(body));
         }
       );
     }
